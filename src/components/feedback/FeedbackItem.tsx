b47a142870c989356ed560b2c734a7f30326d8f4
Franmeza <ing.meza.francisco@gmail.com>
2024-03-11 18:43:35 -0500

    logic to increment the upvote count and toggle the text

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,16 +1,28 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { TFeedbackItem } from "../../utils/types";
+import { useState } from "react";
 
 type FeedbackItemsProps = {
   feedbackItem: TFeedbackItem;
 };
 
 function FeedbackItem({ feedbackItem }: FeedbackItemsProps) {
+  const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setUpvoteCount((prev) => ++prev);
+    e.currentTarget.disabled = true;
+    e.stopPropagation();
+  };
   return (
-    <li className="feedback">
-      <button>
+    <li
+      onClick={() => setOpen(!open)}
+      className={`feedback ${open ? "feedback--expand" : ""}`}
+    >
+      <button onClick={handleUpvote}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
